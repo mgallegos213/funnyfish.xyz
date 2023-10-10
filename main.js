9402d6a069f7d7bf7b841d565236159c04d36177
Michael Gallegos <michael@mgallegos.dev>
2023-10-09 22:21:03 -0400

    adding fish and shark sprites, proper background, better css

--- main.js
+++ main.js
@@ -1,68 +1,84 @@
 import * as THREE from 'three';
 
 let scene, camera, renderer;
-let fish;
-let direction = 1;  // 1 for positive (right), -1 for negative (left)
+let fish, shark;
+let fishDirection = 1;  // 1 for positive (right), -1 for negative (left)
+let sharkDirection = -1; // starts moving to the left
 
 function init() {
-    // Create a new scene
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x0099CC);  // Blue background
 
-    // Setup the camera
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // Setting up the camera
+    camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000); // aspect ratio is 1 for a square canvas
     camera.position.z = 5;
 
-    // Initialize the WebGL renderer
-    renderer = new THREE.WebGLRenderer({ antialias: true });
+    // WebGL renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
-    const container = document.getElementById('app');  // Assuming your main div has the id 'app'
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
+    adjustCanvasSize();
+    document.getElementById('app').appendChild(renderer.domElement);
 
-    // Render the scene with the camera
-    renderer.render(scene, camera);
-
-    // Load the fish sprite texture
-    const texture = new THREE.TextureLoader().load('/fish1.png');
-    texture.center.set(0.5, 0.5);  // Set the pivot point to the center of the texture
-    texture.repeat.set(direction, 1);  // Initial repeat value based on the direction
-
-    const spriteMaterial = new THREE.SpriteMaterial({ map: texture });
-    fish = new THREE.Sprite(spriteMaterial);
+    // Fish sprite
+    const fishTexture = new THREE.TextureLoader().load('/guppy1.png');
+    fishTexture.center.set(0.5, 0.5);
+    fishTexture.repeat.set(fishDirection, 1);
+    const fishMaterial = new THREE.SpriteMaterial({ map: fishTexture });
+    fish = new THREE.Sprite(fishMaterial);
     fish.scale.set(0.8, 0.8, 0.8);
+    fish.position.y = 0.5;  // Adjust for fish
     scene.add(fish);
 
+    // Shark sprite
+    const sharkTexture = new THREE.TextureLoader().load('/shark1.png');
+    sharkTexture.center.set(0.5, 0.5);
+    sharkTexture.repeat.set(-1, 1);
+    const sharkMaterial = new THREE.SpriteMaterial({ map: sharkTexture });
+    shark = new THREE.Sprite(sharkMaterial);
+    shark.material.map.repeat.x = 1;
+    shark.scale.set(-1, 1, 1);
+    shark.position.y = -0.5;  // Adjust for shark
+    shark.position.x = 2.5;   // Starts from the right
+    scene.add(shark);
+    scene.background = null;
     animate();
 }
 
 init();
 
+function adjustCanvasSize() {
+    const size = Math.min(window.innerWidth, window.innerHeight);
+    renderer.setSize(size, size);
+}
+
 function animate() {
     requestAnimationFrame(animate);
 
-    fish.position.x += 0.01 * direction;
-
-    // Check boundaries and change direction
+    // Fish movement
+    fish.position.x += 0.01 * fishDirection;
     if (fish.position.x > 2.5) {
-    direction = -1;
-    fish.material.map.repeat.x = -1;  // Flip the texture horizontally
+        fishDirection = -1;
+        fish.material.map.repeat.x = -1;
     } else if (fish.position.x < -2.5) {
-    direction = 1;
-    fish.material.map.repeat.x = 1;   // Return to original texture orientation
+        fishDirection = 1;
+        fish.material.map.repeat.x = 1;
+    }
+
+    // Shark movement
+    shark.position.x += 0.01 * sharkDirection;
+    if (shark.position.x < -2.5) {
+        sharkDirection = 1;
+        shark.material.map.repeat.x = -1;
+    } else if (shark.position.x > 2.5) {
+        sharkDirection = -1;
+        shark.material.map.repeat.x = 1;
     }
 
     renderer.render(scene, camera);
-    }
+}
 
-    // Handle window resize
-    window.addEventListener('resize', () => {
-    const newWidth = window.innerWidth;
-    const newHeight = window.innerHeight;
-
-    camera.aspect = newWidth / newHeight;
+window.addEventListener('resize', () => {
+    adjustCanvasSize();
+    camera.aspect = 1;
     camera.updateProjectionMatrix();
-
-    renderer.setSize(newWidth, newHeight);
-    renderer.render(scene, camera);  // Re-render after resizing
+    renderer.render(scene, camera);
 });
